03373a837e44f97d72624689efdf2db2108d0b42
teloon <telooon@gmail.com>
2013-09-12 14:01:44 +0800

    change some defaults: port, default todo

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 
 build_path = lambda *a: os.path.join(ROOT, *a)
 
-define("port", default=8888, help="run on the given port", type=int)
+define("port", default=8808, help="run on the given port", type=int)
 define("mongo_host", default="localhost", help="server address of mongodb", type=str)
 define("mongo_port", default=27017, help="port of mongodb server", type=int)
 define("tid_len", default=5, help="the length of shorturl id ", type=int)
